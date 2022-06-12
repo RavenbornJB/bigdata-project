d78040a5373de91bfe6dd0240dc45c23970469e3
RavenbornJB <yarema.mishchenko@gmail.com>
2022-06-12 17:51:35 +0300

    added precomputed reports dummy; merged run scripts

---
+++ precomputed-reports/compute_reports.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+REPORT_HOURS = 6
+
+
+def compute_reports(data, start_hour):
+    """
+    should write reports to 3 json files
+    """
+    return NotImplemented
--- run-cluster.sh
+++ run-cluster.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+echo 'Running Kafka and Zookeeper'
 docker network create project-network
 docker run -d --name zookeeper-server --network project-network -e ALLOW_ANONYMOUS_LOGIN=yes bitnami/zookeeper:latest
 docker run -d --name kafka-server --network project-network -e ALLOW_PLAINTEXT_LISTENER=yes -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper-server:2181 bitnami/kafka:latest
@@ -8,3 +9,18 @@ sleep 10
 
 # decided to create topic in the same script
 docker run -it --rm --network project-network -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper-server:2181 bitnami/kafka:latest kafka-topics.sh --create  --bootstrap-server kafka-server:9092 --replication-factor 1 --partitions 3 --topic wiki-create
+
+
+echo 'Running Cassandra'
+docker run --name cassandra-server --network project-network -d cassandra:latest
+until docker exec cassandra-server cqlsh &> /dev/null
+do
+	sleep 1
+done
+echo 'Node 1 created.'
+
+docker exec cassandra-server mkdir /opt/app
+docker cp ddl.cql cassandra-server:/opt/app
+docker exec cassandra-server cqlsh --file /opt/app/ddl.cql
+
+echo 'DONE.'
